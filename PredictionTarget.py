68c1018a2d20b9d6b82dcaa841959cad08f97764
navarr17 <navarr17@students.rowan.edu>
2025-04-20 19:20:11 -0500

    added Prediction to target nexts days closing price

---
+++ PredictionTarget.py
@@ -0,0 +1,10 @@
+import Indicators as indicate
+# Create target variable - next day's closing price
+indicate.stock_data['Target'] = indicate.stock_data['Close'].shift(-1)
+
+# Remove the last row since it won't have a target
+stock_data = indicate.stock_data.dropna()
+
+# Split features and target
+X = stock_data.drop(['Target'], axis=1)
+y = stock_data['Target']
