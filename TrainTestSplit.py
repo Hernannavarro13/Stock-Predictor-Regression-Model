a5835e8d2c9b54be69e845ee99245739077fd4a5
navarr17 <navarr17@students.rowan.edu>
2025-04-20 19:21:19 -0500

    set training to 80-20 train test to start off and added TrainTestSplit.py

---
+++ TrainTestSplit.py
@@ -0,0 +1,13 @@
+import PredictionTarget as predictor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Determine the split point (80% train, 20% test)
+split_idx = int(len(predictor.X) * 0.8)
+X_train, X_test = predictor.X.iloc[:split_idx], predictor.X.iloc[split_idx:]
+y_train, y_test = predictor.y.iloc[:split_idx], predictor.y.iloc[split_idx:]
+
+# Scale the features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
